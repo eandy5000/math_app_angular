2b9ffe8443307f5f15df527e8a2a2941f53385a3
Andy Eccleston <eandy5000@yahoo.com>
2016-11-02 19:46:38 -0500

    basic server setup

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const server = app.listen(app.get('port'), () => {
+    let port = server.address().port
+    console.log(`listening on port: ${port}`)
+})
+
