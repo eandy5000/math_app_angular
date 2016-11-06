1ba5c265a166286f3a2e47aae28204a76a6014d3
Andy Eccleston <eandy5000@yahoo.com>
2016-11-06 11:40:44 -0600

    replaced jqlite with vanilla js

--- public/app/components/actions/color.js
+++ public/app/components/actions/color.js
@@ -1,9 +1,13 @@
 (function(){
 
     var link = function(scope, el, attr) {
-        el.on('click', function(){
-            el.css('color', '#fff')
-        })
+        var sel = document.querySelector("p")
+        var test = function(){
+            console.log("I work")
+            sel.classList.toggle("color")
+        }
+        sel.onclick = test
+        console.log(sel)
     }
     
     var colorChange = function() {
